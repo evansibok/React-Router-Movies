e71199da68c3bb71fb1530e049f494760b8ae2fb
Evans Ibok <evansibok@gmail.com>
2019-11-12 15:17:11 +0100

    refactored my code to use the MovieCard component from stretch

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
-const MovieList = props => {
+const MovieList = () => {
   const [movies, setMovies] = useState([])
   useEffect(() => {
     const getMovies = () => {
@@ -29,24 +29,8 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2><Link to={`/movies/${movie.id}`}>{title}</Link></h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+    <MovieCard movie={movie}/>
   );
 }
 
